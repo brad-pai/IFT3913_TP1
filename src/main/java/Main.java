345129eb0078a6dbab5b3d3f7957a31a2db0c166
Brad <bradspai@gmail.com>
2022-02-10 19:25:36 -0500

    reading lines and directory content

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,18 @@
+import java.io.FileReader;
+
 public class Main {
 
     public static void main(String[] args) {
+            try {
+                FileReader reader = new FileReader("src/FolderTest/Test.java");
 
+                Parser parser = new Parser();
+                int temp = parser.classLines(reader);
+                System.out.println(temp);
+
+                parser.folderContent();
+            }catch(Exception ex){
+                return;
+            }
     }
 }
